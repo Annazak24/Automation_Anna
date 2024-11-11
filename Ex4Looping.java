017d12c9412265d7a15d67c67dfc0286e6ac3aba
Annazak24 <zaqaryan.anna856@gmail.com>
2024-11-12 00:13:09 +0400

    Looping 4

--- Ex4Looping.java
+++ Ex4Looping.java
@@ -1,3 +1,22 @@
-/*Write a program that takes as an input an integer n and an integer k and calculates n to the power of k (usage of Math.pow() is not allowed).*/
+/*Write a program that takes as an input an integer n and an integer k and
+calculates n to the power of k (usage of Math.pow() is not allowed).*/
 public class Ex4Looping {
+    public static int power(int n, int k) {
+        int power = 1;
+        if (n == 0) {
+            return 0;
+        }
+        else {
+            while (k>0) {
+                power= power*n;
+                    k--;}
+            }
+        return power;
+    }
+
+    public static void main(String[] args) {
+        int power= power(-5,2);
+        System.out.println(power);
+    }
 }
+
