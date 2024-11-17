4f1e4f37feba0904d5a49c86682bd5f02b02aa67
Annazak24 <zaqaryan.anna856@gmail.com>
2024-11-17 23:39:28 +0400

    Looping  7,8

--- Ex7Looping.java
+++ Ex7Looping.java
@@ -1,3 +1,28 @@
-/*Write a program that takes as an input a positive integer n and computes the natural logarithm of 2, by adding up to n terms in the series: 1 - 1/2 + 1/3 - 1/4 + 1/5 -... 1/n.*/
+/*Write a program that takes as an input a positive integer n and computes the natural
+logarithm of 2, by adding up to n terms in the series: 1 - 1/2 + 1/3 - 1/4 + 1/5 -... 1/n. */
 public class Ex7Looping {
+    public static double Logarithm(int n) {
+
+        if (n <= 0) {
+        }
+
+        double sum = 0.0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 2 == 0) {
+                sum -= 1.0 / i;
+            } else {
+                sum += 1.0 / i;
+            }
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        int n = 10;
+        double logarithm = Logarithm(n);
+
+
+        System.out.println("The Logarithm of " + n + "  is: " + logarithm);
+    }
+}
+
 }
--- Ex8Looping.java
+++ Ex8Looping.java
@@ -1,3 +1,20 @@
-/*Write a program that takes as an input an integer number representing half of the number of rows and displays diamond-like patterns.*/
+/*Write a program that takes as an input an integer number representing half of the number
+ of rows and displays diamond-like patterns.*/
 public class Ex8Looping {
+    public static double computeLogarithm(int n) {
+        double sum = 0;
+        for (int i = 1; i <= n; i++) {
+            sum += (i % 2 == 0 ? -1.0 : 1.0) / i;
+        }
+
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        double pattern = computeLogarithm(n);
+        System.out.println(pattern);
+    }
 }
+
+
