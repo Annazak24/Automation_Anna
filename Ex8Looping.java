ec875aa85ef6c71062b7dd703c8a1963f82f6e8a
Annazak24 <zaqaryan.anna856@gmail.com>
2024-11-18 21:57:47 +0400

    Looping  8

--- Ex8Looping.java
+++ Ex8Looping.java
@@ -1,20 +1,25 @@
 /*Write a program that takes as an input an integer number representing half of the number
  of rows and displays diamond-like patterns.*/
 public class Ex8Looping {
-    public static double computeLogarithm(int n) {
+    public static double logarithm(int n) {
         double sum = 0;
         for (int i = 1; i <= n; i++) {
-            sum += (i % 2 == 0 ? -1.0 : 1.0) / i;
+            if (i % 2 == 0) {
+                sum -= 1.0 / i;
+            } else {
+                sum += 1.0 / i;
+            }
         }
 
         return sum;
     }
 
     public static void main(String[] args) {
-        int n = 10;
-        double pattern = computeLogarithm(n);
-        System.out.println(pattern);
+        int n = 10; // Number of terms to calculate in the series
+        double result = logarithm(n); // Call the logarithm method
+        System.out.println("Result of the logarithmic series for n = " + n + " is: " + result);
     }
 }
+
 
 
